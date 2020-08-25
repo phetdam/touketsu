115f20a044ad853ca5c1e224be26d533332c7088
Derek Huang <djh458@stern.nyu.edu>
2020-08-25 00:36:25 -0400

    add comment explaining relative import

--- touketsu/tests/__init__.py
+++ touketsu/tests/__init__.py
@@ -1,4 +1,4 @@
 __doc__ = "``__init__.py`` for ``touketsu.tests`` module."
 
-# decorators to give
+# decorators to give classes nice sklearn-like __repr__methods + utilities
 from .repr import get_simple_repr_verbosity, has_simple_repr, srepr, vrepr
